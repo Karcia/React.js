c12f84253ae3288e5325dea41abd92a44414f2a4
Karcia <karciawow@gmail.com>
2017-08-17 15:05:06 +0200

    moved App content to Index file,deleted App file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import KanbanBoard from './KanbanBoard';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App';
 
+let cardsList = [
+  {
+    id: 1,
+    title: "read book",
+    description: "description",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "write some code",
+    description: "description",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "name",
+        done: true
+      },
+      {
+        id: 2,
+        name: "name",
+        done: false
+      },
+      {
+        id: 3,
+        name: "name",
+        done: false
+      }
+    ]
+  },
+];
+
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
 registerServiceWorker();
